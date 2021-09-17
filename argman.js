466ae09498095a5fca92fb7386808bab240dfc71
windingtheropes <17016045+windingtheropes@users.noreply.github.com>
2021-09-17 10:36:53 -0400

    Update argman.js

--- argman.js
+++ argman.js
@@ -1,18 +1,3 @@
-module.exports.getArgs = (preArgs = false) => {
-    switch(preArgs)
-    {
-        case true:
-            return getArgsArray()
-            break;
-        case false:
-            const a = getArgsArray()
-            a.shift()
-            a.shift()
-            return a
-            break;
-        default:
-            throw new Error('preArgs is not a boolean.')
-            return
-            break;
-    }
+module.exports.getArgs = () => {
+   return process.argv
 }
